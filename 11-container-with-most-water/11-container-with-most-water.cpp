e4ff936730eb001bdbb7416ec261305e27b5dac5
Gopal M A <gopalrmd90@gmail.com>
2022-01-30 19:46:28 +0530

    Time: 112 ms (49.94%), Space: 59 MB (80.46%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int max = 0;
-        int n = height.size();
         int i , j;
-        for ( i = 0, j = n-1; i < j;  ){
+        for ( i = 0, j = height.size()-1; i < j;  ){
             int minval = (height[i] <= height[j]) ? height[i]: height[j];
             int range = j - i;
            // cout << "range = " << range <<endl;
