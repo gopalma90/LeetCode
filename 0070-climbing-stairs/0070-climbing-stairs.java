7cc7b46d8b460d9c668c7cbe84a2d42dcc3a83b7
Gopal M A <gopalrmd90@gmail.com>
2026-06-16 14:50:13 +1200

    Time: 0 ms (100%), Space: 42 MB (53.28%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+  public int climbStairs (int n)
+  {
+    int []array = new int[n+3];
+    array[0] = 0;
+    array[1] = 1;
+    
+    array[2] = 2;
+
+    for ( int i = 3; i <= n; i++)
+    {
+      array[i] = array[i-1] + array[i-2];
+    }
+    return array[n];
+
+  }
+}
